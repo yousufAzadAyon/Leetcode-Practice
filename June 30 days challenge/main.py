ac2cf814f98744f2da0280e50f7dba091feb24a2
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-03 16:36:30 +0600

    Day Three

--- June 30 days challenge/main.py
+++ June 30 days challenge/main.py
@@ -32,3 +32,20 @@ class SolutionTwo:
         """
         node.val = node.next.val
         node.next = node.next.next
+
+
+# Day Three
+
+class SolutionThree:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        sorted_cost = sorted(costs, key= lambda x:x[0] - x[1])
+        result = 0
+        
+        for i in range(len(costs)):
+            if i < len(costs)/2:
+                result += sorted_cost[i][0]
+                
+            else:
+                result += sorted_cost[i][1]
+                
+        return result
