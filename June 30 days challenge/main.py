e27f0c552d61005680503048bbab44ec27905a7d
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-04 14:31:15 +0600

    day four. easy one

--- June 30 days challenge/main.py
+++ June 30 days challenge/main.py
@@ -49,3 +49,25 @@ class SolutionThree:
                 result += sorted_cost[i][1]
                 
         return result
+
+
+    # Day Four
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+        # without build in function
+
+        # def reverseStringWithoutFunction(s):
+        #     i = 0 
+        #     j = len(s) - 1 
+
+        #     while i<j:
+        #         s[i], s[j] = s[j], s[i]
+        #         i += 1
+        #         j -= 1
+
