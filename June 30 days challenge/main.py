c077c6bf93afc0f24d8f44e6ab2c59cf202d34d0
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-05 22:35:21 +0600

    day five. question was poorly worded.

--- June 30 days challenge/main.py
+++ June 30 days challenge/main.py
@@ -71,3 +71,34 @@ class Solution:
         #         i += 1
         #         j -= 1
 
+        # day five
+
+class SolutionFive:
+
+    def __init__(self, w: List[int]):
+        self.w = []
+        total = 0
+        for weight in w:
+            total += weight
+            self.w.append(total)
+        self.total = total 
+        
+
+    def pickIndex(self) -> int:
+        #from random import random
+
+        random_number = self.total * random.random()      #works without import
+        low, high = 0, len(self.w)
+        while low < high: 
+            mid = low + (high - low) // 2
+            if random_number > self.w[mid]:
+                low = mid + 1 
+            else:
+                high = mid
+                
+        return low
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
+
