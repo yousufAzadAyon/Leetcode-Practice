1c8f99fc386079fff8beb962cacaa53d6c1a2fef
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-06 18:13:12 +0600

    added alternative for day six

--- June 30 days challenge/main.py
+++ June 30 days challenge/main.py
@@ -128,3 +128,16 @@ class SolutionSix:
                 
         return result
 
+        # easy alternative for day Six
+
+class SolutionSixAlternative:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people = sorted(people, key= lambda x: (-x[0], x[1]))
+        
+        result = []
+        
+        for person in people:
+            result.insert(person[1],person)
+            
+        return result
+
