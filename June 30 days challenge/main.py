c069f4e98a53146473be0eb6842f00095be6bc3c
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-11 14:56:04 +0600

    week two day 4

--- June 30 days challenge/main.py
+++ June 30 days challenge/main.py
@@ -223,3 +223,38 @@ class SolutionTen:
             if nums[i] >= target:
                 return i
 
+
+        # week two day four 
+
+class SolutionEleven:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        start = 0
+        end = len(nums)-1
+        
+        while i <= end:
+            if nums[i] == 1:
+                i+=1
+            elif nums[i] == 0:
+                if i == start:
+                    i+=1
+                else:
+                    nums[start], nums[i] = nums[i], nums[start]
+                start+=1
+            else:
+                nums[end], nums[i] = nums[i], nums[end]
+                end-=1
+
+
+        # using python builtin function 
+
+class SolutionElevenBuiltinFunction:
+    def sortColors(self, nums):
+        nums.sort()
+
+        # easy but not efficient 
+
+
