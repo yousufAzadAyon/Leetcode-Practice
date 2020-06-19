d3c42e85fe7620fe6dcf6bb30fcd9e51434c99dc
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-19 18:01:43 +0600

    week three day 5

--- June 30 days challenge/week_three.py
+++ June 30 days challenge/week_three.py
@@ -99,3 +99,43 @@ class SolutionFour:
                 beg = mid + 1
         return n - beg
 
+            # week three day five
+
+class SolutionFive:
+    def RabinKarp(self,text, M, q):
+        if M == 0: return True
+        h,t,d = 1,0,256
+
+        dic = defaultdict(list)
+        for i in range(M-1):  h = (h * d) % q 
+
+        for i in range(M): 
+            t = (d * t + ord(text[i]))% q
+
+        dic[t].append(i-M+1)
+
+        for i in range(len(text) - M):
+            t = (d*(t-ord(text[i])*h) + ord(text[i + M]))% q
+            dic[t].append(i+1)
+
+        for key in dic:
+            for i,j in combinations(dic[key], 2):
+                if text[i:i+M] == text[j:j+M]:
+                    return (True, text[i:i+M])
+
+        return (False, "")
+
+
+    def longestDupSubstring(self, S):
+        beg, end = 0, len(S)
+        q = (1<<31) - 1 
+        Found = ""
+        while beg + 1 < end:
+            mid = (beg + end)//2
+            isFound, candidate = self.RabinKarp(S, mid, q)
+            if isFound:
+                beg, Found = mid, candidate
+            else:
+                end = mid
+
+        return Found
