f0b783ba969987625e33610815a3214617b4b214
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-20 17:57:40 +0600

    week three day 6

--- June 30 days challenge/week_three.py
+++ June 30 days challenge/week_three.py
@@ -139,3 +139,29 @@ class SolutionFive:
                 end = mid
 
         return Found
+
+
+        # week three day six
+
+class SolutionSix:
+    def getPermutation(self, n: int, k: int) -> str:
+        fact = [1]
+        num = [1]
+        i=1;
+        while(i < n):
+            num.append(i+1)
+            a  = fact.pop(len(fact)-1)
+            fact.append(a)
+            fact.append(a*i)
+            i+=1
+        i=1
+        str_n = 0
+        k-=1
+        while(i <= n):
+            index = int((k)/fact[n-i])
+            str_n = 10*str_n + num[index]
+            num.remove(num[index])
+            k %=fact[n-i]
+            i+=1
+        return str(str_n)
+
