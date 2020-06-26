a10bf6839831d4ec9f318af3eece64ae4422e316
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-26 14:20:56 +0600

    week four day 5

--- June 30 days challenge/week_four.py
+++ June 30 days challenge/week_four.py
@@ -61,3 +61,17 @@ class SolutionFour:
         for i in sor:
             if sor[i] == sor[i-1]:
                 return sor[i]
+
+        # week four day five
+
+class SolutionFive:
+    def sumNumbers(self, root):
+        if not root: return 0
+        
+        if not root.left and not root.right:
+            return int(root.val)
+        
+        if root.left: root.left.val = 10*root.val + root.left.val
+        if root.right: root.right.val = 10*root.val + root.right.val
+            
+        return self.sumNumbers(root.left) + self.sumNumbers(root.right)
