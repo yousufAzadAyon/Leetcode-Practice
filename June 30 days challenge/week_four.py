e7dfdc3d750787baa81692a8bef901129f05f16b
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-27 15:32:40 +0600

    week four day 6

--- June 30 days challenge/week_four.py
+++ June 30 days challenge/week_four.py
@@ -75,3 +75,32 @@ class SolutionFive:
         if root.right: root.right.val = 10*root.val + root.right.val
             
         return self.sumNumbers(root.left) + self.sumNumbers(root.right)
+
+
+        # week four day six
+
+class SolutionSix:
+    def numSquares(self, n: int) -> int:
+        arr, i = [], 1
+        while i**2 <= n:
+            arr.append(i**2)
+            i += 1
+        
+        #one-sum O(N^(1/2))
+        if n in arr:
+            return 1
+        
+        #two-sum O(N)
+        for e in arr:
+            if n-e in arr:
+                return 2
+        
+        #three-sum O(N)
+        arr_set = set(arr)
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if n-arr[i]-arr[j] in arr_set:
+                    return 3
+        
+        #four-sum O(1)
+        return 4
