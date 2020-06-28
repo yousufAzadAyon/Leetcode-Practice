0234dc86e3a5f7fc0f90e8883a6ad75766f23445
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-06-28 16:31:20 +0600

    week four day 7

--- June 30 days challenge/week_four.py
+++ June 30 days challenge/week_four.py
@@ -104,3 +104,22 @@ class SolutionSix:
         
         #four-sum O(1)
         return 4
+
+        # week four day seven 
+
+class SolutionSeven:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        def dfs(dep):
+            arr = paths[dep]
+            while arr:
+                dfs(arr.pop())
+            res.append(dep)
+
+        res = []
+        paths = defaultdict(list)
+        tickets.sort(key=lambda x: x[1], reverse=True)
+        for s, t in tickets:
+            paths[s].append(t)
+        dfs('JFK')
+        return res[::-1]
+
