f91e7c26c80b4aa5f5646fdecdd7c866e6e6e54e
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-08-06 18:06:48 +0600

    Week One Day Six

---
+++ August LeetCoding Challenge/Week One/Find All Duplicates in an Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        seen = {}
+        result = []
+        for i in nums:
+            if i not in seen:
+                seen[i] = 1
+            else:
+                seen[i] += 1
+                
+        for i,j in seen.items():
+            if j != 1:
+                result.append(i)
+                
+        return result 
