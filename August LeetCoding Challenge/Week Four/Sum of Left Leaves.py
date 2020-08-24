f6d0e5022afd3312e77d6fd2890cb4cc0af476ee
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-08-24 14:58:49 +0600

    Week Four Day Three

---
+++ August LeetCoding Challenge/Week Four/Sum of Left Leaves.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        result = 0
+        stack = [(root, False)]
+        while stack:
+            curr, is_left = stack.pop()
+            if not curr:
+                continue
+            if not curr.left and not curr.right:
+                if is_left:
+                    result += curr.val
+            else:
+                stack.append((curr.left, True))
+                stack.append((curr.right, False))
+        return result
