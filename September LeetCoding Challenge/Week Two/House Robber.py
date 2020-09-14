5da0f73650c97da692435b85ea2658a6a615678b
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-09-14 14:45:39 +0600

    Week Two Day Seven

---
+++ September LeetCoding Challenge/Week Two/House Robber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums):
+        n = len(nums)
+        dp = [0] * (n + 2)
+        for i in range(n):
+            dp[i + 2] = max(dp[i + 1], dp[i] + nums[i])
+        
+        return dp[-1]
