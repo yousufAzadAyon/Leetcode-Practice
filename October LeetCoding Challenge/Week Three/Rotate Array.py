7458d7903e2dedf07b7e939dade308ce936908b8
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-10-15 19:19:54 +0600

    Week Three Day One

---
+++ October LeetCoding Challenge/Week Three/Rotate Array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        if len(nums) < 2:
+            return
+        
+        n = len(nums)
+        k = k % n
+        
+        if k == 0:
+            return
+        
+        self.reverse(nums, 0, n-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, n-1)
+        
+    
+    def reverse(self, nums, l, r):
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l+=1
+            r-=1
