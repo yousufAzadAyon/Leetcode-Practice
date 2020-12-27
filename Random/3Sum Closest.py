b86c4bcfbd35ac3d13f35900ab5796d9f316bfa4
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2020-12-27 19:08:23 +0600

    👣 3Sum Closest

---
+++ Random/3Sum Closest.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSumClosest(self, nums, target):
+        if not nums:
+            return None
+
+        diff=float('inf')
+        nums.sort()
+        
+        for i in range(len(nums)-2):
+            p1=i+1
+            p2=len(nums)-1
+            
+            while p1<p2:
+                s=nums[i]+nums[p1]+nums[p2]
+                if abs(s-target) < diff:
+                    diff=abs(s-target)
+                    output=s
+                
+                if s<target:
+                    p1+=1
+                elif s>target:
+                    p2-=1
+                else:
+                    return target
+        return output      
