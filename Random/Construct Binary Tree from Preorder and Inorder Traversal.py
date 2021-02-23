5bff9f136d90166885a91e16f289a70db9e7c4ef
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-02-23 19:16:37 +0600

    Construct Binary Tree from...

---
+++ Random/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        if not preorder:
+            return None
+        if not inorder:
+            return None
+
+        ele = preorder.pop(0)
+        root = TreeNode(ele)
+        idx = inorder.index(ele)
+        root.left = self.buildTree(preorder, inorder[:idx])
+        root.right = self.buildTree(preorder, inorder[idx+1:])
+
+        return root
