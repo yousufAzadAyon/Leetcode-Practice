a8c92c0deb8107e1ac465fa08c728c2a381bd6da
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-03-15 04:34:37 +0600

    Pascals Triangle

---
+++ Random/Pascals-Triangle.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generate(self, numRows):
+        res = []
+        for i in range(1, numRows+1):
+            l = []
+            for j in range(1, i+1):
+                if j==1 or j==i:
+                    l.append(1)
+                else:
+                    l.append(res[-1][j-i-1]+res[-1][j-i])
+
+            res.append(l)
+        return res
+
