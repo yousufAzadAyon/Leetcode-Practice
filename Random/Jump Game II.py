55c71f268460e1b05b3ca80ec73a385f7554e304
yousufAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-04-07 22:28:55 +0600

    Jump Game II

---
+++ Random/Jump Game II.py
@@ -0,0 +1,22 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        cur, tmp = 0, 0
+        jump_count = 0
+        length = len(nums)
+        while cur < length - 1:
+            cur_max = 0
+            for i in range(1, nums[cur]+1):
+                # Whenever reach end, return jump+1
+                if cur+i >= length-1:
+                    return (jump_count+1)
+                # Find out the furthest position
+                elif cur + i + nums[cur+i] >= cur_max:
+                    cur_max = cur + i + nums[cur+i]
+                    tmp = cur + i
+            # If the furthest position is bigger than length, return jump+2
+            if cur_max >= length-1:
+                return (jump_count+2)                  
+            jump_count += 1
+            cur = tmp
+        
+        return jump_count
