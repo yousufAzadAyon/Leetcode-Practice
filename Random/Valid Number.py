2696f3e48da0b36e98bdbf31d91fd30c09d54f9f
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-04-27 13:41:50 +0600

    valid number

---
+++ Random/Valid Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        try:
+            if 'inf' in s.lower() or s.isalpha():
+                return False
+            if float(s) or float(s) >= 0:
+                return True
+        except:
+            return False
