583e5a3795c6b6700e0d9214bc2f6262475fa808
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-05-21 22:12:42 +0600

    Week Three Day Six

---
+++ May LeetCoding Challenge 2021/Week Three/Binary Tree Level Order Traversal.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: TreeNode):
+        
+        if not root:
+            return 
+        else:
+            s1 = []  # Using queue
+            
+            res = []
+            
+            s1.append(root)
+            
+            while len(s1)>0:
+                
+                l=[]
+                
+                for i in range(len(s1)):
+                    temp = s1.pop(0)
+                    
+                    l.append(temp.val)
+                    
+                    if temp.left:
+                        s1.append(temp.left)
+                    
+                    if temp.right:
+                        s1.append(temp.right)
+                
+                res.append(l)
+            return res
