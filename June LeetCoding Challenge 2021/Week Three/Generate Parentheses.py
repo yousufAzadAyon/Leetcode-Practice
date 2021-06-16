829389a27b21dee790461a8742b92320e45564a0
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-06-16 17:22:27 +0600

    week 3 day 2

---
+++ June LeetCoding Challenge 2021/Week Three/Generate Parentheses.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.ans = []
+        self.dfs('', n, n)
+        return self.ans
+
+    def dfs(self, pre, left, right):
+        if right == 0:
+            self.ans.append(pre)
+        if left:
+            self.dfs(pre + '(', left - 1, right)
+        if right > left:
+            self.dfs(pre + ')', left, right - 1)
