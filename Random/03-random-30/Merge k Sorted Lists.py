880dc2f26365edfb5b14857f6068c0a70f71b609
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-06-30 10:00:55 +0600

    refactor

--- Random/03-random-30/Merge k Sorted Lists.py
+++ Random/03-random-30/Merge k Sorted Lists.py
@@ -2,29 +2,25 @@
 from typing import List
 import heapq
 
+
 class ListNode:    
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        
         def getElem(head):
             temp = []
-            
             while head:
                 temp.append(head.val)
                 head = head.next
-            
             return temp
-        
         semifinal = []
-        
         for x in range(len(lists)):
             semifinal.append(getElem(lists[x]))
-        
         final = list(heapq.merge(*semifinal))
-		
         if final:
             root = ListNode(final[0])
             node = root
@@ -32,5 +28,4 @@ class Solution:
             for x in range(len(final)-1):
                 node.next = ListNode(final[x+1])
                 node = node.next
-
             return root
