77ec7767395d5b3b64535800318f56d8517e993e
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-07-07 21:13:18 +0600

    week one day seven

---
+++ July LeetCoding Challenge 2021/Week One/Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def kthSmallest(self, matrix, k):
+        n, beg, end = len(matrix), matrix[0][0], matrix[-1][-1]
+        
+        def check(m):
+            i, j, cnt = 0, n-1, 0
+            for i in range(n):
+                while j >= 0 and matrix[i][j] > m: j -= 1
+                cnt += (j + 1)
+            return cnt
+         
+        while beg < end:
+            mid = (beg + end)//2
+            if check(mid) < k:
+                beg = mid + 1
+            else:
+                end = mid
+                
+        return beg
