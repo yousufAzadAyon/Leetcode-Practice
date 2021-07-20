af9190775d275e2a17085d6d08f087a48d4a5c7a
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-07-20 21:13:31 +0600

    week three day six

---
+++ July LeetCoding Challenge 2021/Week Three/Shuffle an Array.py
@@ -0,0 +1,22 @@
+from random import randint
+class Solution:
+    def __init__(self, nums):
+        self.nums = nums[:]
+        self.copy = nums[:]
+
+    def reset(self):
+        self.nums = self.copy[:]
+        return self.nums
+        
+    def shuffle(self):
+        n = len(self.nums)
+        for i in range(n):
+            j = randint(i, n - 1)
+            self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
+        return self.nums
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
